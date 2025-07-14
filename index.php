623f8f724c9f0a8b968f8e1248654c1ad06a3933
leemutai <mutailee361@gmail.com>
2025-07-14 13:15:14 +0300

    Added dynamic sidebar navigation based on user role using PHP

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Dashboard</title>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    />
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <input type="checkbox" id="checkbox" />
+    <header class="header">
+      <h2 class="u-name">
+        SIDE <b>BAR</b>
+        <label for="checkbox">
+          <i id="navbtn" class="fa fa-bars" aria-hidden="true"></i>
+        </label>
+      </h2>
+      <i class="fa-solid fa-bell"></i>
+    </header>
+
+    <div class="body">
+      <nav class="side-bar">
+        <div class="user-p">
+          <img src="img/prt.png" alt="" />
+          <h4>Lee</h4>
+        </div>
+        
+         <?php 
+         $user = "admin";  
+         if ($user == "employee") {
+          # code...
+         
+         ?>
+         <!-- User Navigation Bar -->
+        <ul>
+          <li>
+            <a href="#">
+              <i class="fa fa-tachometer" aria-hidden="true"></i>
+              <span>Dashboard</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i class="fa fa-tasks" aria-hidden="true"></i>
+              <span>My Task</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i class="fa fa-user" aria-hidden="true"></i>
+              <span>Profile</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i class="fa fa-bell" aria-hidden="true"></i>
+              <span>Notifications</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i class="fa fa-sign-out" aria-hidden="true"></i>
+              <span>Logout</span>
+            </a>
+          </li>
+        </ul>
+        <?php } else { ?>
+        <!-- Admin Navigation Bar -->
+         <ul>
+          <li>
+            <a href="#">
+              <i class="fa fa-tachometer" aria-hidden="true"></i>
+              <span>Dashboard</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i class="fa fa-users" aria-hidden="true"></i>
+              <span>Manage Users</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i class="fa fa-plus" aria-hidden="true"></i>
+              <span>Create Tasks</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i class="fa fa-tasks" aria-hidden="true"></i>
+              <span>All Tasks</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i class="fa fa-bell" aria-hidden="true"></i>
+              <span>Notifications</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i class="fa fa-sign-out" aria-hidden="true"></i>
+              <span>Logout</span>
+            </a>
+          </li>
+        </ul>
+          <?php } ?>
+      </nav>
+      <section class="section-1">
+        <h1>WELCOME</h1>
+        <p>#CodingWithLee</p>
+      </section>
+    </div>
+  </body>
+</html>
